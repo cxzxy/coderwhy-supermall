588d64461130fd39cbbcc66224a70c55ab726244
cxzxy <2460456193@qq.com>
2022-07-14 21:13:42 +0800

    目录准备工作结束

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,18 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// const { defineConfig } = require('@vue/cli-service')
+// //const { resolve } = require('core-js/fn/promise')
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
+module.exports = {
+    configureWebpack: {
+        resolve: {
+            alias: {
+                'assets': '@/assets',
+                'common': '@/common',
+                'components': '@/components',
+                'network': '@/network',
+                'views': '@/views',
+            }
+        }
+    }
+}n
